cc7cf09c4a9d8f2eb53d9e321923cab7c27173e0
allenz0810 <lingjiez321@gmail.com>
2016-10-30 13:02:06 -0400

    read from appsetting
    read from appsetting

--- src/CustomeMiddleware/CustomMidware.cs
+++ src/CustomeMiddleware/CustomMidware.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
 
 namespace CustomeMiddlewarem
 {
     public class CustomMidware
     {
         private readonly RequestDelegate _next;
-        private readonly static string _basepath = "";
-        private readonly static string _filepath = "";
+        private static string EmailFrom;
+        private static string EmailTo;
         private readonly IConfigurationRoot _config;
+
         public CustomMidware(IConfigurationRoot config, RequestDelegate next)
         {
             _config = config;
+            EmailFrom = _config["ErrorSetUp:Email:Form"];
+            //(_config["ConnectionStrings:WorldContextConnection"]
+            EmailTo = _config["ErrorSetUp:Email:To"];
             _next = next;
         }
 
         public async Task Invoke(HttpContext httpContext)
         {
-            
             Console.WriteLine($"Request for {httpContext.Request.Path} received ({httpContext.Request.ContentLength ?? 0} bytes)");
-            
+
             await _next.Invoke(httpContext);
-            
         }
 
         //public static void SetBasePath(string basePath)
