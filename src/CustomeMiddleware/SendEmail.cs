821a754327cad2314d26a8a637c15af3bc440c98
allenz0810 <lingjiez321@gmail.com>
2016-10-31 22:22:00 -0400

    add email class
    add email class

---
+++ src/CustomeMiddleware/SendEmail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace CustomeMiddleware
+{
+    public static class SendEmail
+    {
+        public static void Send()
+        {
+            using (var client = new HttpClient())
+            {
+                //client.
+                //    .Connect("smtp.example.com", 587, false);
+                //client.AuthenticationMechanisms.Remove("XOAUTH2");
+                //// Note: since we don't have an OAuth2 token, disable 	// the XOAUTH2 authentication mechanism.     client.Authenticate("anuraj.p@example.com", "password");
+                //client.Send(message);
+                //client.Disconnect(true);
+            }
+        }
+    }
+}
